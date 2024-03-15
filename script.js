f911442d061cddc1af22dbd53c9131ed7aa5ee15
JoseAngelCh <jose.angel2605@outlook.com>
2024-03-15 17:04:45 -0400

    JS

---
+++ script.js
@@ -0,0 +1,29 @@
+
+// const miConstante = `hola mundo, hola todos`
+
+// console.log(miConstante)
+
+// const list = [
+//     `aceite`,
+//     `arroz`,
+//     `aceite`,
+//     `cafe`   
+// ]
+// console.log(list)
+
+
+// const usuario = {
+//     name: `jose`,
+//     age: 26,
+//     password: 1234,
+//     job: `informatic`
+// }
+// console.log(usuario.name)
+
+// let variable = true 
+// if (variable) {
+//     console.log(`Se Cumplio`)
+// } else {
+//     console.log(`No se sumple`)
+// }
+
